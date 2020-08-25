4ebc5a0b21358f6f334405c34ba99eb20c8815db
Evandro Falleiros <evandro.falleiros@ifms.edu.br>
2020-08-25 15:45:19 -0400

    limpeza index

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import fetch from 'isomorphic-unfetch'
-import styled from '@emotion/styled';
 
 export default function Home(props) {
   return (
-    <AppStyled>
+    <>
       <Head>
         <title>{props.geral.nomeApp}</title>
         <link rel="icon" href={props.geral.favicon.url} />
       </Head>
-    </AppStyled>
+    </>
   )
 }
 
 export async function getStaticProps(context) {
-  const geral = await fetch('http://guaruak.herokuapp.com/geral');
-  const geralData = await geral.json();
+  const geralReq = await fetch('http://guaruak.herokuapp.com/geral');
+  const geral = await geralReq.json();
 
   return {
     props: {
-      geral: geralData
+      geral
     }
   }
 }
 
-const AppStyled = styled.div``;
+
