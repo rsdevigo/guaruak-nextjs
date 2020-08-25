d1d1ad993a121dd90bb9faa6f2f90ad928f2b76b
rsdevigo <rsdevigo@gmail.com>
2020-08-25 11:27:23 -0400

    Primeiro Commit

---
+++ pages/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Header = () => {
+  return <HeaderStyled></HeaderStyled>;
+};
+
+const HeaderStyled = styled.header``;
+
+export default Header;
