9f3f09001f82fec652e9738943e15909e17e1f78
Evandro Falleiros <evandro.falleiros@ifms.edu.br>
2020-08-25 14:31:24 -0400

    default theming

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import "../styles/globals.css";
+import { ThemeProvider } from 'emotion-theming'
+import theme from '../themes/default.js';
+import GlobalStyles from './components/GlobalStyles/GlobalStyles.js';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return( 
+    <> 
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp;
---
+++ themes/default.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+      verde: "#527A05",
+      amarelo: "#FFC43E",
+      laranja: "#ffc43e"
+    },
+    spacing: {
+      containerSize: "80%",
+      defaultPadding: "1rem"
+    }
+}
+
+export default theme;
